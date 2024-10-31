55ccbf1edfdb7a7c00c40c241a21b3e5556d9702
Anton Mamontov <a.mamontov@p-s.kz>
2024-10-31 12:47:25 +0500

    without trush lines

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,6 @@ import './App.css';
 import img from './assets/Kiss.png'
 import kissSound from './assets/kiss-sound.mp3'
 import bottleSound from './assets/spinning-sound.mp3'
-// import Bottle from './components/Bottle';
-// import images from '../public/image/1.png'
-// import {im} from '../public/image'
-
-
-// const playersData = Array.from({ length: 10 }, (_, i) => ({
-//   id: i,
-//   name: `Player ${i + 1}`,
-//   avatar: `https://randomuser.me/api/portraits/men/${i + 1}.jpg`,
-//   isMale: true
-// }));
 
 const playersArray = [
   {
@@ -50,12 +39,6 @@ const playersArray = [
 
 
 const App = () => {
-
-  // console.log(im)
-
-  console.log(playersArray)
-
-  // const [players] = useState(playersData);
   const [players] = useState(playersArray);
   const [activePlayerIndex, setActivePlayerIndex] = useState(0);
   const [kissCounter, setKissCounter] = useState(0);
@@ -65,17 +48,12 @@ const App = () => {
   const [countdown, setCountdown] = useState(3);
   const [isCountdownActive, setIsCountdownActive] = useState(false);
   const [isKissing, setIsKissing] = useState(false);
-  // const [bottleAngle, setBottleAngle] = useState(0)
   const [bottleIndex, setBottleIndex] = useState(0)
+  const [circleRadius, setCircleRadius] = useState(200);
 
-
-  const [circleRadius, setCircleRadius] = useState(200); // Начальный радиус
-
-  // Обновляем радиус в зависимости от ширины экрана
   useEffect(() => {
     const updateRadius = () => {
       const minDimension = Math.min(window.innerWidth, window.innerHeight);
-      console.log('w',window.innerWidth)
       setCircleRadius((minDimension * 0.6) / 2); 
     };
 
@@ -104,7 +82,6 @@ const App = () => {
     startBottle()
   }
 
-  // Запуск обратного отсчёта и последующее вращение
   const startSpin = () => {
     setIsCountdownActive(true);
     setCountdown(3);
@@ -113,7 +90,7 @@ const App = () => {
       setCountdown(prev => {
         if (prev === 1) {
           clearInterval(countdownInterval);
-          spinBottle(); // Запуск вращения после обратного отсчёта
+          spinBottle();
           setIsCountdownActive(false);
           return 4;
         }
@@ -121,7 +98,7 @@ const App = () => {
       });
     }, 1000);
   };
-// Функция для выбора случайного игрока (кроме активного)
+
 const getRandomPlayerIndex = (excludeIndex, maxPlayers) => {
   let index;
   do {
@@ -130,29 +107,16 @@ const getRandomPlayerIndex = (excludeIndex, maxPlayers) => {
   return index;
 };
 
-  
-
-
-  // Функция для вращения бутылки
   const spinBottle = () => {
     setIsSpinning(true);
     const nextPlayerIndex = getRandomPlayerIndex(activePlayerIndex, players.length);
-    // setTargetPlayerIndex(nextPlayerIndex);
-
     const anglePerPlayer = 360 / players.length;
-    // console.log('anglePerPlayer', anglePerPlayer)
     const targetAngle = nextPlayerIndex * anglePerPlayer;
-    // console.log('targetAngle', targetAngle)
 
     const spins = 4 * 360;
     setBottleIndex(nextPlayerIndex * 36 + 90)
     setRotationAngle(spins + targetAngle + 90);
     
-    // setBottleAngle(targetAngle)
-    // console.log('rrrbottle', rotationAngle)
-    // console.log('botle', bottleIndex)
-    // console.log('minus', rotationAngle - bottleIndex)
-
     setTimeout(() => {
       setIsSpinning(false);
      
@@ -161,14 +125,11 @@ const getRandomPlayerIndex = (excludeIndex, maxPlayers) => {
       setIsKissing(true);
       setTargetPlayerIndex(nextPlayerIndex);
       
-      // setBottleIndex(nextPlayerIndex * 36 + 90)
       const spins = 4 * 360;
       setRotationAngle(spins + bottleIndex);
       
-      // Завершаем поцелуй и обновляем активного игрока
       setTimeout(() => {
         setActivePlayerIndex(nextPlayerIndex);
-        // setKissCounter(prev => prev + 1);
         setIsKissing(false);
       }, 2000);
     }, 4000);
@@ -182,23 +143,20 @@ const getRandomPlayerIndex = (excludeIndex, maxPlayers) => {
       <div><img className={`kiss-image ${isKissing && 'kiss-visible'}`} src={img} alt="" /></div>
       {isCountdownActive && <div className="countdown">{countdown}</div>}
       <div className="players-circle">
-        {players.map((player, index) => {
-          const angle = (360 / players.length) * index;
-          const x = circleRadius * Math.cos((angle * Math.PI) / 180) - 40;
-          const y = circleRadius * Math.sin((angle * Math.PI) / 180) - 40;
-
-          return (
-            
-            <div
-              key={player.id}
-              className={`player-circle ${index === activePlayerIndex ? 'active' : ''} ${index === targetPlayerIndex ? 'target' : ''} ${isKissing && (index === activePlayerIndex || index === targetPlayerIndex) ? 'kissing' : ''}`}
-              style={{ transform: `translate(${x}px, ${y}px)` }}
-            >
-              {/* <img src={player.avatar} alt={player.name} /> */}
-              <img src={`../kissstoner/public/image/${player.id}.png`} alt={player.id} />
-            </div>
-          );
-        })}
+          {players.map((player, index) => {
+            const angle = (360 / players.length) * index;
+            const x = circleRadius * Math.cos((angle * Math.PI) / 180) - 40;
+            const y = circleRadius * Math.sin((angle * Math.PI) / 180) - 40;
+            return (   
+              <div
+                key={player.id}
+                className={`player-circle ${index === activePlayerIndex ? 'active' : ''} ${index === targetPlayerIndex ? 'target' : ''} ${isKissing && (index === activePlayerIndex || index === targetPlayerIndex) ? 'kissing' : ''}`}
+                style={{ transform: `translate(${x}px, ${y}px)` }}
+              >
+                <img src={`../kissstoner/public/image/${player.id}.png`} alt={player.id} />
+              </div>
+            );
+          })}
       </div>
       <div className="bottle-container">
         <div
@@ -212,14 +170,6 @@ const getRandomPlayerIndex = (excludeIndex, maxPlayers) => {
           onClick={startSpin}
         ></div>
       </div>
-      {/* <Bottle
-      isCountdownActive={isCountdownActive}
-      countdown={countdown}
-        isSpinning={isSpinning}
-        rotationAngle={rotationAngle}
-        bottleIndex={bottleIndex}
-        startSpin={startSpin}
-      /> */}
     </div>
   );
 };
